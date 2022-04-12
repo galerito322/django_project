ce121fe9f2f1bdac4ed80eff19a2ed5afa1cd6a5
galerito322 <lucasbragacruz@outlook.com>
2022-04-12 02:52:32 -0300

    herance in templates and template block

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from recipes.views import home
+#from recipes.views import home
+from . import views
+
+# . significa algo como caminho irmão. (da pasta que eu estou import views)
+views
 
 urlpatterns = [
-    path('', home),
+    path('', views.home),
+    path('recipes/<int:id>/', views.recipes),
 ]
