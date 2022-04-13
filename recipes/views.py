9dfbe4374bec9767f94f0232c3473214033fc183
galerito322 <lucasbragacruz@outlook.com>
2022-04-13 02:48:39 -0300

    injecting data in list of home view with factory.py

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,14 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Lucas',
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 
 def recipes(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Lucas',
+        'recipe': make_recipe(),
     })
