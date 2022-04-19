4cee77b4c8fea441bef5424af5ef08f30472f77e
galerito322 <lucasbragacruz@outlook.com>
2022-04-19 02:33:01 -0300

    redone all files to fix test file errors

--- recipes/views.py
+++ recipes/views.py
@@ -34,7 +34,9 @@ def category(request, category_id):
 
 def recipe(request, id):
 
-    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
+    recipe = get_object_or_404(
+        Recipe, pk=id, is_published=True
+    )
 
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
